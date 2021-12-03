6c2673d6966825eef715a4f5fa6445afa5783e7a
Carolyn Qu <67798872+carolynqu@users.noreply.github.com>
2021-12-03 16:12:18 -0500

    Update youbot_controller.py
    psuedo code

--- youbot_controller.py
+++ youbot_controller.py
@@ -46,6 +46,46 @@ def turn_right(wheels, SPEED):
 def turn_left(wheels, SPEED):
     speeds = [SPEED, -SPEED, SPEED, -SPEED]
     set_speeds(wheels, speeds)
+    
+
+#CAMERA RGB to HSV conversion
+
+def rgbToHSV(r, g, b):
+ 
+    # R, G, B values are divided by 255
+    # to change the range from 0..255 to 0..1:
+    r, g, b = r / 255.0, g / 255.0, b / 255.0
+ 
+    # h, s, v = hue, saturation, value
+    cmax = max(r, g, b)    # maximum of r, g, b
+    cmin = min(r, g, b)    # minimum of r, g, b
+    diff = cmax-cmin       # diff of cmax and cmin.
+ 
+    # if cmax and cmax are equal then h = 0
+    if cmax == cmin:
+        h = 0
+     
+    # if cmax equal r then compute h
+    elif cmax == r:
+        h = (60 * ((g - b) / diff) + 360) % 360
+ 
+    # if cmax equal g then compute h
+    elif cmax == g:
+        h = (60 * ((b - r) / diff) + 120) % 360
+ 
+    # if cmax equal b then compute h
+    elif cmax == b:
+        h = (60 * ((r - g) / diff) + 240) % 360
+ 
+    # if cmax equal zero
+    if cmax == 0:
+        s = 0
+    else:
+        s = (diff / cmax) * 100
+ 
+    # compute v
+    v = cmax * 100
+    return h, s, v
 
 
 
@@ -176,6 +216,31 @@ def main():
         
         # initiate wheels
         wheels = [fr, fl, br, bl] 
+        
+        
+        #PSEUDO CODE
+        
+        if safe_berry:
+            position berry camera to the middle
+             
+            if robot_info[0] < 80 OR robot_info[1] < 70:
+               go_forward(wheels, 16.0)
+            else:
+                go_forward(wheels, 10.0)
+        
+        if moving forward:
+            if section2clear and section3clear:
+                go_forward(wheels, 10.0, 10.0)
+            else:
+                processSurrounding()
+        
+        if processSurrounding State:
+            if berryExists with No Zombies:
+                
+                switchtocase SafeBerries
+            
+        
+        
 
         # values = gyro.getValues()
 
